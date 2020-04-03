c2cae6cc2973527b097ad555a5ded1ed65d389af
Dakota <xeliusone@gmail.com>
2020-04-03 16:17:49 -0600

    Setting up the logger

--- src/main.rs
+++ src/main.rs
@@ -14,5 +14,8 @@ pub struct Pong;
 impl SimpleState for Pong {}
 
 fn main() -> amethyst::Result<()> {
+	//logs errors, warnings, and debug messages
+	amethyst::start_logger(Default::default());
+	
 	Ok(())
 }
