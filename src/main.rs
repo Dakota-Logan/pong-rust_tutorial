e04a4788f550467538aa80bd97371a052eae0697
Dakota <xeliusone@gmail.com>
2020-04-03 16:44:36 -0600

    Creating an application

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 //!Pong Tutorial 1
 use amethyst::{
 	prelude::*,
-	rederer::{
-		plugins::{RenderFlat2d, RenderTowindow},
+	renderer::{
+		plugins::{RenderFlat2D, RenderToWindow},
 		types::DefaultBackend,
 		RenderingBundle,
 	},
@@ -20,7 +20,16 @@ fn main() -> amethyst::Result<()> {
 	let app_root = application_root_dir()?;
 	let display_config_path = app_root.join("config").join("display.ron");
 	
+	//Creating a new instance of GameDataBuilder, a central repository of all the game logic that runs periodically during the game runtime.
+	let game_data = GameDataBuilder::default();
 	
+	//
+	let assets_dir = app_root.join("assets");
+	//Combining the builder with the game struct (Pong), creating the overarching Amethyst's root object: Application.
+	//It (Application) binds the OS event loop, state machines, timers and other core components in a central place.
+	let mut game = Application::new(assets_dir, Pong, game_data)?;
+	//Starting the game loop.
+	game.run();
 	
 	Ok(())
 }
